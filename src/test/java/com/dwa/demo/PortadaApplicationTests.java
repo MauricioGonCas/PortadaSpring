aa3b47a810e4a5edbb214b67dfbbf87c33aa4ebd
MauricioGonCas <mauriciogonzalezcas@gmail.com>
2021-05-20 16:03:15 -0500

    mensaje

---
+++ src/test/java/com/dwa/demo/PortadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
